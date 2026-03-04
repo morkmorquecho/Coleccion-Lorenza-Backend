0262f5b2114173ab626be683fd54ad808ed0a8f7
morkmorquecho <mork.morquecho@gmail.com>
2026-03-04 15:32:00 -0600

    doc(blog): add doc

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 
+from blog.doc.schema import BLOG_VIEWSET
 from blog.models import Blog
 from blog.serializer import BlogSerializer
 from core.mixins import ViewSetSentryMixin
@@ -8,7 +9,9 @@ from core.permission import IsAdminOrReadOnly
 from pieces.models import Piece
 
 # Create your views here.
+@BLOG_VIEWSET
 class BlogViewSet(ViewSetSentryMixin, ModelViewSet):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
     permission_classes = [IsAdminOrReadOnly]
+    lookup_field = 'slug'

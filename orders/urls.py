385e9f555b82356c57e034ff1ceaede6a0f70a67
morkmorquecho <mork.morquecho@gmail.com>
2026-06-04 11:13:37 -0600

    delete debug view

--- orders/urls.py
+++ orders/urls.py
@@ -8,7 +8,7 @@ router.register(r'orders',OrderViewSet, basename='orders')
 router.register(r'shipping-trackings', ShippingTrackingViewSet, basename='shipping-tracking')
 
 orders_patterns = [
-    path('debug/cache/', CacheDebugView.as_view(), name='debug cache'),
+    # path('debug/cache/', CacheDebugView.as_view(), name='debug cache'),
     path('orders/checkout/', CheckoutView.as_view(), name='checkout'),
     path('orders/webhook/', StripeWebhookView.as_view(), name='stripe-webhook'),
     path('orders/<int:pk>/cancel/', CancelOrderView.as_view(), name='cancel-order'),
